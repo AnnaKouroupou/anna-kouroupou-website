2bff761363f017461c34101566a8c8c5ecae5f21
apzelos <a.p.zelos@gmail.com>
2021-04-20 19:26:35 +0300

    fix footer socials

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,10 +3,18 @@
     <div class="container">
       <span v-html="settings.footer" />
       <div class="footer-socials">
-        <span>facebook</span>
-        <span>instagram</span>
-        <span>pinterest</span>
-        <span>youtube</span>
+        <a :href="settings.youtube_url"
+          ><font-awesome style="color: #c4302b" :icon="['fab', 'youtube']"
+        /></a>
+        <a :href="settings.facebook_url"
+          ><font-awesome style="color: #3b5998" :icon="['fab', 'facebook']"
+        /></a>
+        <a :href="settings.instagram_url"
+          ><font-awesome style="color: #bc2a8d" :icon="['fab', 'instagram']"
+        /></a>
+        <a :href="settings.pinterest_url"
+          ><font-awesome style="color: #e60023" :icon="['fab', 'pinterest']"
+        /></a>
       </div>
     </div>
   </footer>
@@ -29,10 +37,29 @@ export default {
 }
 .footer .container {
   display: flex;
+  align-items: center;
   justify-content: space-between;
+  flex-direction: column-reverse;
+  width: 100%;
 }
-.footer-socials span {
-  margin-left: 1rem;
-  font-weight: bold;
+.footer-socials {
+  margin-bottom: 2rem;
+  font-size: 1.4rem;
+}
+.footer-socials a:not(:first-of-type) {
+  margin-left: 1.4rem;
+}
+
+@media (min-width: 860px) {
+  .footer-socials {
+    margin-bottom: 0;
+    font-size: 1.2rem;
+  }
+  .footer-socials a:not(:first-of-type) {
+    margin-left: 0.8rem;
+  }
+  .footer .container {
+    flex-direction: row;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import DefaultLayout from "~/layouts/Default.vue";
 import settings from "../data/theme.json";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { config, library } from '@fortawesome/fontawesome-svg-core'
+import { faFacebook, faInstagram, faYoutube, faPinterest } from '@fortawesome/free-brands-svg-icons'
 
 import "@/assets/code-highlight.css"
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+config.autoAddCss = false;
+library.add( faFacebook, faInstagram, faYoutube, faPinterest )
 
 export default function(Vue, { head }) {
   Vue.component("Layout", DefaultLayout);
+  Vue.component('font-awesome', FontAwesomeIcon)
+
   head.bodyAttrs = {
     class: settings.dark_mode ? "dark" : ""
   };
